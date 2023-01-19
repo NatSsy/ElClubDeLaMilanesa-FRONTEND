af54ccf147c1f24adbff31602cd5d9b939487e6b
Mauro Ybañez <maurogabriely@gmail.com>
2023-01-19 17:31:35 -0300

    creacion componente Menus

---
+++ src/components/Menus.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Menus = () => {
+  return (
+    <div>Menus
+      <section>Card Entradas</section>
+      <section>Card plato Principal</section>
+      <section>Card postres</section>
+    </div>
+  )
+}
