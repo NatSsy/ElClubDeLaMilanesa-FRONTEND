08b84282611e26c856a8caeacc5d3c189e66c39e
Miguel Montoro <miguelmontoro1997@gmail.com>
2023-03-13 13:24:58 -0300

    prototipo de estilos de aboutPage

--- src/ClubMilanesa/pages/AboutPage.jsx
+++ src/ClubMilanesa/pages/AboutPage.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const AboutPage = () => {
   return (
-    <></>
+    <div>
+      <div className="container">
+        <div className="card">
+          <img src="" alt="Imagen 1"/>
+          <div className="info">
+            <h2>Título 1</h2>
+            <p>Descripción 1</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
