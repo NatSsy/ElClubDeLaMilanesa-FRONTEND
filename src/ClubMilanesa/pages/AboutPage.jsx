053899748d589ec2282849a4eeb4b073bfb04f9b
Miguel Montoro <miguelmontoro1997@gmail.com>
2023-03-27 21:44:23 -0300

    desarrollo de detalles

--- src/ClubMilanesa/pages/AboutPage.jsx
+++ src/ClubMilanesa/pages/AboutPage.jsx
@@ -3,7 +3,64 @@ import React from 'react'
 const AboutPage = () => {
   return (
     <div>
-      <div className="container">
+        <div className="texto">
+            <h1 className="titulo-abaut text-center">ACERCA DE NOSOTROS</h1>
+            <p className="intro text-center">
+                Recordá: No se puede comprar la felicidad. Pero si una sanguche de milanesa que es casi lo mismo.
+            </p>
+        </div>
+    <div className="container">
+      <div>
+        <div className="card">
+          <img src="" alt="Imagen 1"/>
+          <div className="info">
+            <h2>Título 1</h2>
+            <p>Descripción 1</p>
+          </div>
+        </div>
+      </div>
+    
+      <div>
+        <div className="card">
+          <img src="" alt="Imagen 1"/>
+          <div className="info">
+            <h2>Título 1</h2>
+            <p>Descripción 1</p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="card">
+          <img src="" alt="Imagen 1"/>
+          <div className="info">
+            <h2>Título 1</h2>
+            <p>Descripción 1</p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="card">
+          <img src="" alt="Imagen 1"/>
+          <div className="info">
+            <h2>Título 1</h2>
+            <p>Descripción 1</p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="card">
+          <img src="" alt="Imagen 1"/>
+          <div className="info">
+            <h2>Título 1</h2>
+            <p>Descripción 1</p>
+          </div>
+        </div>
+      </div>
+
+      <div>
         <div className="card">
           <img src="" alt="Imagen 1"/>
           <div className="info">
@@ -13,7 +70,7 @@ const AboutPage = () => {
         </div>
       </div>
     </div>
-
+    </div>
     
   )
 }
