757096cbacc470ca898c777c989c31584c11be1a
Miguel Montoro <miguelmontoro1997@gmail.com>
2023-04-01 20:57:36 -0300

    detalles de estilos-bootstrap

--- src/ClubMilanesa/pages/AboutPage.jsx
+++ src/ClubMilanesa/pages/AboutPage.jsx
@@ -5,72 +5,74 @@ const AboutPage = () => {
     <div>
         <div className="texto">
             <h1 className="titulo-abaut text-center">ACERCA DE NOSOTROS</h1>
-            <p className="intro text-center">
+            <p className="intro text-center text-white">
                 Recordá: No se puede comprar la felicidad. Pero si una sanguche de milanesa que es casi lo mismo.
             </p>
         </div>
     <div className="container">
-      <div>
-        <div className="card">
+      <div className='row card-container'>
+      
+        <div className="card col-2">
           <img src="" alt="Imagen 1"/>
           <div className="info">
             <h2>Título 1</h2>
             <p>Descripción 1</p>
           </div>
         </div>
-      </div>
+      
     
-      <div>
-        <div className="card">
-          <img src="" alt="Imagen 1"/>
-          <div className="info">
-            <h2>Título 1</h2>
-            <p>Descripción 1</p>
-          </div>
-        </div>
-      </div>
 
-      <div>
-        <div className="card">
+        <div className="card col-2">
           <img src="" alt="Imagen 1"/>
           <div className="info">
             <h2>Título 1</h2>
             <p>Descripción 1</p>
           </div>
         </div>
-      </div>
+  
 
-      <div>
-        <div className="card">
-          <img src="" alt="Imagen 1"/>
-          <div className="info">
-            <h2>Título 1</h2>
-            <p>Descripción 1</p>
-          </div>
-        </div>
-      </div>
 
-      <div>
-        <div className="card">
+        <div className="card col-2">
           <img src="" alt="Imagen 1"/>
           <div className="info">
             <h2>Título 1</h2>
             <p>Descripción 1</p>
           </div>
         </div>
-      </div>
+  
 
-      <div>
-        <div className="card">
+
+        <div className="card col-2">
           <img src="" alt="Imagen 1"/>
           <div className="info">
             <h2>Título 1</h2>
             <p>Descripción 1</p>
           </div>
         </div>
-      </div>
+  
+
+
+        <div className="card col-2">
+          <img src="" alt="Imagen 1"/>
+          <div className="info">
+            <h2>Título 1</h2>
+            <p>Descripción 1</p>
+          </div>
+        </div>
+  
+
+
+        <div className="card col-2">
+          <img src="" alt="Imagen 1"/>
+          <div className="info">
+            <h2>Título 1</h2>
+            <p>Descripción 1</p>
+          </div>
+        </div>
+  
     </div>
     </div>
+  </div>
     
   )
 }
