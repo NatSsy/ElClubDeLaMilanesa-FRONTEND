49de702829f92e916b9b31b629d397038526312d
Miguel Montoro <miguelmontoro1997@gmail.com>
2023-04-10 17:22:53 -0300

    agregando imagenes correccion es estilos

--- src/ClubMilanesa/pages/AboutPage.jsx
+++ src/ClubMilanesa/pages/AboutPage.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import nata from "../../assets/img/about/nata.jpeg"
+import miguel from "../../assets/img/about/miguel.jpeg"
+import brendan from "../../assets/img/about/brendan.jpeg"
+import mauro from "../../assets/img/about/mauro.jpeg"
+import nacho from "../../assets/img/about/nacho.jpeg"
 
 const AboutPage = () => {
   return (
@@ -12,24 +17,24 @@ const AboutPage = () => {
     <div className="container">
       <div className='row card-container'>
 
-        <div className="card col-2">
-          <img src="" alt="Imagen 1"/>
+        {/* <div className="card col-2">
+          <img src={} alt="Imagen 1"/>
           <div className="info">
             <h2>Juan Aguera</h2>
             <p>Descripción 1</p>
           </div>
-        </div>
+        </div> */}
       
         <div className="card col-2">
-          <img src="" alt="Imagen 1"/>
+          <img src={miguel} alt="Imagen 2"/>
           <div className="info">
             <h2>Miguel Montoro</h2>
-            <p>Descripción 1</p>
+            <p>"La mente es el poder maestro que crea y moldea. Y el hombre es mente…"</p>
           </div>
         </div>
   
         <div className="card col-2">
-          <img src="" alt="Imagen 1"/>
+          <img src={brendan} alt="Imagen 3"/>
           <div className="info">
             <h2>Brendan Heindl</h2>
             <p>Descripción 1</p>
@@ -37,7 +42,7 @@ const AboutPage = () => {
         </div>
   
         <div className="card col-2">
-          <img src="" alt="Imagen 1"/>
+          <img src={nata} alt="Imagen 4"/>
           <div className="info">
             <h2>Natanael SS</h2>
             <p>Descripción 1</p>
@@ -45,7 +50,7 @@ const AboutPage = () => {
         </div>
   
         <div className="card col-2">
-          <img src="" alt="Imagen 1"/>
+          <img src={mauro} alt="Imagen 5"/>
           <div className="info">
             <h2>Mauro Ibañez</h2>
             <p>Descripción 1</p>
@@ -53,10 +58,10 @@ const AboutPage = () => {
         </div>
   
         <div className="card col-2">
-          <img src="" alt="Imagen 1"/>
+          <img src={nacho} alt="Imagen 6"/>
           <div className="info">
             <h2>Ignacio Sierra</h2>
-            <p>Descripción 1</p>
+            <p>"Hay 2 días en los que no puedes hacer nada, uno es ayer y el otro es mañana"</p>
           </div>
         </div>
   
