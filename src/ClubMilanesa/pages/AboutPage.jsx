2fde81cbd389ec1b94d44201303ebf2933cc7d65
Miguel Montoro <miguelmontoro1997@gmail.com>
2023-04-10 15:04:55 -0300

    arreglando detalles

--- src/ClubMilanesa/pages/AboutPage.jsx
+++ src/ClubMilanesa/pages/AboutPage.jsx
@@ -5,67 +5,57 @@ const AboutPage = () => {
     <div>
         <div className="texto">
             <h1 className="titulo-abaut text-center">ACERCA DE NOSOTROS</h1>
-            <p className="intro text-center text-white">
+            <p className="intro text-center text-white py-4">
                 Recordá: No se puede comprar la felicidad. Pero si una sanguche de milanesa que es casi lo mismo.
             </p>
         </div>
     <div className="container">
       <div className='row card-container'>
-      
+
         <div className="card col-2">
           <img src="" alt="Imagen 1"/>
           <div className="info">
-            <h2>Título 1</h2>
+            <h2>Juan Aguera</h2>
             <p>Descripción 1</p>
           </div>
         </div>
       
-    
-
         <div className="card col-2">
           <img src="" alt="Imagen 1"/>
           <div className="info">
-            <h2>Título 1</h2>
+            <h2>Miguel Montoro</h2>
             <p>Descripción 1</p>
           </div>
         </div>
   
-
-
         <div className="card col-2">
           <img src="" alt="Imagen 1"/>
           <div className="info">
-            <h2>Título 1</h2>
+            <h2>Brendan Heindl</h2>
             <p>Descripción 1</p>
           </div>
         </div>
   
-
-
         <div className="card col-2">
           <img src="" alt="Imagen 1"/>
           <div className="info">
-            <h2>Título 1</h2>
+            <h2>Natanael SS</h2>
             <p>Descripción 1</p>
           </div>
         </div>
   
-
-
         <div className="card col-2">
           <img src="" alt="Imagen 1"/>
           <div className="info">
-            <h2>Título 1</h2>
+            <h2>Mauro Ibañez</h2>
             <p>Descripción 1</p>
           </div>
         </div>
   
-
-
         <div className="card col-2">
           <img src="" alt="Imagen 1"/>
           <div className="info">
-            <h2>Título 1</h2>
+            <h2>Ignacio Sierra</h2>
             <p>Descripción 1</p>
           </div>
         </div>
